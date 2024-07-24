b210236b5bf6e32b4fc35dbf7c72eca646ae46fe
CengizHTW <93160498+CengizHTW@users.noreply.github.com>
2024-07-24 02:47:10 +0200

    Restart and Menu

--- Assets/Scenes/Features/TImer/Timer.cs
+++ Assets/Scenes/Features/TImer/Timer.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
     private float timer = 0.0f;
     public TMP_Text timerText; // Referenz zum Text-Element im UI
-
-    void Start()
-    {
-    }
+    public GameManagerScript gameManager;
+    private bool isDead;
+   
 
     void Update()
     {
         timer += Time.deltaTime;
         timerText.text = "Timer: " + timer.ToString("F2") + " Sekunden";
+        if (timer > 3 && !isDead)
+        {
+            isDead = true;
+            SceneManager.LoadScene(1);
+            Debug.Log("dead");
+        }
     }
 }
